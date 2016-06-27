e3f98b9247fde06b3dbee0e5e533b6dfbd8f4758
j <j@ravellaw.com>
2016-06-27 04:07:45 -0700

    WIP: persisting options.
    http://stackoverflow.com/a/37539241
    Security wrapper denied access to property "storedOptions" on privileged Javascript object. Support for exposing privileged objects to untrusted content via __exposedProps__ is being gradually removed - use WebIDL bindings or Components.utils.cloneInto instead. Note that only the first denied property access from a given global object will be reported.

--- popup/word-cloud-config.js
+++ popup/word-cloud-config.js
@@ -1,24 +1,68 @@
-function getOptions() {
+const STORAGE_KEY = 'storedOptions';
+
+function minWordSizeInput() {
+  return document.getElementById('minWordSizeInput');
+}
+function filterWordsTextarea() {
+  return document.getElementById('filterWordsTextarea');
+}
+function phraseSizeInput() {
+  return document.getElementById('phraseSizeInput');
+}
+function minFontSizeInput() {
+  return document.getElementById('minFontSizeInput');
+}
+function maxFontSizeInput() {
+  return document.getElementById('maxFontSizeInput');
+}
+function svgWidth() {
+  return document.getElementById('svgWidth');
+}
+function svgHeight() {
+  return document.getElementById('svgHeight');
+}
+
+function getPopupOptions() {
   return {
     wordOptions: {
-      minWordSize: Number(document.getElementById('minWordSizeInput').value),
-      ignoreWords: document.getElementById('filterWordsTextarea').value.trim().split(/\W+/),
-      phraseSize: Number(document.getElementById('phraseSizeInput').value)
+      minWordSize: Number(minWordSizeInput().value),
+      ignoreWords: filterWordsTextarea().value.trim().split(/\W+/),
+      phraseSize: Number(phraseSizeInput().value)
     },
     generatorOptions: {
-      minFontSize: Number(document.getElementById('minFontSizeInput').value),
-      maxFontSize: Number(document.getElementById('maxFontSizeInput').value),
-      svgWidth: Number(document.getElementById('svgWidth').value),
-      svgHeight: Number(document.getElementById('svgHeight').value)
+      minFontSize: Number(minFontSizeInput().value),
+      maxFontSize: Number(maxFontSizeInput().value),
+      svgWidth: Number(svgWidth().value),
+      svgHeight: Number(svgHeight().value)
     }
   };
 }
 
-document.addEventListener("click", function(e) {
-  if (e.target.id != "generateButton") {
-    return;
-  }
+function getStoredOptions(callback) {
+  chrome.storage.local.get(STORAGE_KEY, (res) => {
+    callback(res[STORAGE_KEY]);
+  });
+}
 
+function syncPopupOptions() {
+  getStoredOptions((options) => {
+    if(!!options) {
+      let {
+        wordOptions:{minWordSize, ignoreWords, phraseSize},
+        generatorOptions:{minFontSize, maxFontSize, svgWidth, svgHeight}
+      } = options;
+      minFontSizeInput().value = minWordSize;
+      filterWordsTextarea().value = ignoreWords.join("\n");
+      phraseSizeInput().value = phraseSize;
+      minFontSizeInput().value = minFontSize;
+      maxFontSizeInput().value = maxFontSize;
+      svgWidth().value = svgWidth;
+      svgHeight().value = svgHeight;
+    }
+  });
+}
+
+function generateWordCloud(options) {
   chrome.tabs.executeScript(null, {
     file: "/word-cloud-helper.js"
   });
@@ -26,7 +70,7 @@ document.addEventListener("click", function(e) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     let tab = tabs[0];
     chrome.tabs.sendMessage(tab.id, {
-      options: getOptions(),
+      options: options,
       selector: 'body>:not(svg)'
     }, (svgBlob) => {
       let url = URL.createObjectURL(svgBlob);
@@ -34,4 +78,20 @@ document.addEventListener("click", function(e) {
       window.close();
     });
   });
+}
+
+document.addEventListener("click", function(e) {
+  if (e.target.id == "generateButton") {
+    let options = getPopupOptions();
+    let storageOptions = {};
+    storageOptions[STORAGE_KEY]= options;
+    chrome.storage.local.set(storageOptions);
+    generateWordCloud(options);
+  } else {
+    return;
+  }
+});
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  syncPopupOptions();
 });
